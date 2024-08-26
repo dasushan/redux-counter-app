b0ad9ce3b359169727f36bc89042507dbd3cb951
dasushan <53913896+dasushan@users.noreply.github.com>
2024-08-26 23:04:42 +0530

    subscribing and dispatching actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
 import {createStore} from 'redux'
 
-const counterReducer = (state = {counter: 0}, action) => {
+const initialState = { counter: 0, showCounter: true}
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment') {
         return {
-            counter: state.counter + 2
+            counter: state.counter + 2,
+            showCounter: state.showCounter
         }
     }
 
     if(action.type === 'decrement') {
         return{
-            counter: state.counter - 2
+            counter: state.counter - 2,
+            showCounter: state.showCounter
+        }
+    }
+    if(action.type === 'increase') {
+        return{
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+    if(action.type === 'decrease'){
+        return{
+            counter: state.counter - action.amount,
+            showCounter: state.showCounter
+        }
+    }
+    if(action.type === 'toggle'){
+        return{
+            counter: state.counter,
+            showCounter: !state.showCounter
         }
     }
 
